948d3e64efb4fe924940c2e2a7293dd401ba6b5f
darshanagi <164138130+darshanagi@users.noreply.github.com>
2026-03-13 19:57:26 +0530

    Add files via upload

---
+++ Script1.sh
@@ -0,0 +1,12 @@
+echo "We are trying this practical from linux EC2"
+echo "We are from batch-25 and learning git/gihub"
+echo "We are learning Branching"
+echo "I am learning Devops"
+echo "Adding new line"
+echo "I am from Devops and cloud team"
+
+echo "Mentor:Darshan"
+echo "I am learning Devops and cloud team"
+echo "I am learning Devops and cloud team"
+echo "I am learning Devops and cloud team"
+echo "this is extra line of code"
